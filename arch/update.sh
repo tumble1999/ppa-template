db00b6d5fdeaa865e6013322c81a3246b71756c8
Tumble <tumblegamer@gmail.com>
2021-08-15 15:21:59 +0100

    arch ppa

--- arch/update.sh
+++ arch/update.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-for a in $(ls *.pkg.tar.gz)
+NAME=cinnabar
+PPA=.db.tar.gz
+EXT=.pkg.tar.zst
+
+
+if [ "$(cat /etc/os-release | grep ^ID | sed 's/ID=//g')" != "arch" ]
+then
+	sudo docker build . -t cinnabar-ppa
+	sudo docker run --rm cinnabar-ppa tar -cC /app/ppa . | tar -xC .
+	exit
+fi
+
+if [ -f Packages ]
+then
+	rm Packages
+fi
+
+for a in $(ls *$EXT)
 do
- repo-add cinnabar.db.tar.gz $a
+pacman -Qip $a >>Packages
+ repo-add $NAME$PPA $a
 done
 
-ln -s cinnabar.db.tar.gz cinnabar
+ln -s $NAME$PPA $NAME
+
+if [ "$1"=="docker" ]
+then
+	mkdir ppa
+	cp {Packages,$NAME,$NAME$PPA} ppa
+fi
